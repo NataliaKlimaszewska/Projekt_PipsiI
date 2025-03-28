ee697ed2300e49ee452544c29fbb803a5fad0225
NataliaKlimaszewska <nataliaklimaszewska03@proton.me>
2025-03-28 20:32:42 +0100

    meet the team fixed

--- resources/views/OurTeam.php
+++ resources/views/OurTeam.php
@@ -8,187 +8,70 @@
 </head>
 <body>
 <style>
-    .responsive-cell-block {
-        min-height: 75px;
-    }
-
     * {
         font-family: Nunito, sans-serif;
     }
 
-    .text-blk {
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 0px;
-        margin-left: 0px;
-        line-height: 25px;
-        padding-top: 0px;
-        padding-right: 0px;
-        padding-bottom: 0px;
-        padding-left: 0px;
-    }
-
-    .responsive-container-block {
-        min-height: 75px;
-        height: fit-content;
-        width: 100%;
-        padding-top: 10px;
-        padding-right: 10px;
-        padding-bottom: 10px;
-        padding-left: 10px;
-        display: flex;
-        flex-wrap: wrap;
-        margin-top: 0px;
-        margin-right: auto;
-        margin-bottom: 0px;
-        margin-left: auto;
-        justify-content: flex-start;
-    }
-
     .outer-container {
-        padding-top: 10px;
-        padding-right: 30px;
-        padding-bottom: 10px;
-        padding-left: 30px;
+        padding: 20px;
         background-color: rgb(255, 235, 234);
+        text-align: center;
     }
 
     .heading-text {
         font-weight: 700;
-        font-size: 48px;
-        line-height: 35px;
+        font-size: 36px;
         color: rgb(51, 51, 51);
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 32px;
-        margin-left: 0px;
-        text-align: center;
+        margin-bottom: 20px;
     }
 
     .sub-heading-text {
-        max-width: 470px;
-        font-size: 25px;
-        line-height: 35px;
-        text-align: center;
+        font-size: 20px;
         font-weight: 700;
         color: rgb(102, 102, 102);
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 100px;
-        margin-left: 0px;
-        margin: 0 0 70px 0;
-    }
-
-    .inner-container {
-        flex-direction: column;
-        align-items: center;
-        margin-top: 80px;
-        margin-right: 0px;
-        margin-bottom: 50px;
-        margin-left: 0px;
+        margin-bottom: 40px;
     }
 
     .cards-container {
-        max-width: 1320px;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+        flex-wrap: wrap;
     }
 
-    .name {
-        font-size: 22px;
-        line-height: 35px;
-        font-weight: 700;
-        color: rgb(102, 102, 102);
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 5px;
-        margin-left: 0px;
-    }
-
-    .position {
-        color: rgb(244, 152, 146);
-        font-size: 22px;
-        line-height: 35px;
-        font-weight: 700;
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 20px;
-        margin-left: 0px;
+    .card-container {
+        text-align: center;
+        width: 250px;
     }
 
     .team-member-image {
         width: 100%;
-        margin-top: 0px;
-        margin-right: 0px;
-        margin-bottom: 40px;
-        margin-left: 0px;
+        height: auto;
+        border-radius: 10px;
     }
 
-    .social-media-icon {
-        margin-top: 0px;
-        margin-right: 25px;
-        margin-bottom: 0px;
-        margin-left: 0px;
-        cursor: pointer;
-        margin: 0 25px 10px 0;
+    .name {
+        font-size: 18px;
+        font-weight: 700;
+        color: rgb(102, 102, 102);
+        margin-top: 10px;
     }
 
-    .card-container {
-        padding-top: 0px;
-        padding-right: 25px;
-        padding-bottom: 0px;
-        padding-left: 25px;
-        margin: 0 0 30px 0;
-    }
-
-    @media (max-width: 1024px) {
-        .socialMediaIcons {
-            margin: 0 20px 10px 0;
-        }
+    .position {
+        color: rgb(244, 152, 146);
+        font-size: 18px;
+        font-weight: 700;
     }
 
     @media (max-width: 768px) {
-        .card-container {
-            margin: 0 0 60px 0;
-            width: 80%;
-        }
-
         .cards-container {
             flex-direction: column;
             align-items: center;
-            justify-content: center;
-        }
-    }
-
-    @media (max-width: 500px) {
-        .inner-container {
-            padding: 10px 0 10px 0;
-            margin: 50px 0 50px 0;
-        }
-
-        .cards-container {
-            padding: 10px 0 10px 0;
-        }
-
-        .card-container {
-            width: 100%;
-            padding: 0 15px 0 15px;
-        }
-
-        .outer-container {
-            padding: 10px 20px 10px 20px;
-        }
-
-        .sub-heading-text {
-            font-size: 20px;
-            line-height: 25px;
-            margin: 0 0 30px 0;
-        }
-
-        .heading-text {
-            font-size: 22px;
-            line-height: 28px;
         }
     }
 </style>
+</body>
+<body>
 <nav>
     <a href="home.php">Home</a>
     <a href="CreateRecipe.php">Create Your Own Recipe</a>
@@ -198,48 +81,30 @@
     <a href="Quiz.php">Quiz Page</a>
     <a href="OurTeam.php">Meet Creators</a>
 </nav>
-<h1>Meet Creators</h1>
-<p>Tutaj damy nas :)</p>
-<div class="responsive-container-block outer-container">
-    <div class="responsive-container-block inner-container">
-        <p class="text-blk heading-text">
-           Meet Creators :)
-        </p>
-        <p class="text-blk sub-heading-text">
-            There are your "baking heros"! They made this website for you, so now you don't have to worry about your recipes.
-        </p>
-        <div class="responsive-container-block cards-container">
-            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
-                <p class="text-blk name">
-                    Maxwell Doe
-                </p>
-                <p class="text-blk position">
-                    Instructor
-                </p>
-                <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors1.svg">
-            </div>
-            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
-                <p class="text-blk name">
-                    Maxwell Doe
-                </p>
-                <p class="text-blk position">
-                    Instructor
-                </p>
-                <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors2.svg">
-            </div>
-            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
-                <p class="text-blk name">
-                    Maxwell Doe
-                </p>
-                <p class="text-blk position">
-                    Instructor
-                </p>
-                <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg">
 
-            </div>
+<div class="outer-container">
+    <h1 class="heading-text">Meet Creators :)</h1>
+    <p class="sub-heading-text">
+        These are your "baking heroes"! They created this website so you don't have to worry about your recipes.
+    </p>
+
+    <div class="cards-container">
+        <div class="card-container">
+            <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors1.svg" alt="Instructor">
+            <p class="name">Maxwell Doe</p>
+            <p class="position">Instructor</p>
+        </div>
+        <div class="card-container">
+            <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors2.svg" alt="Instructor">
+            <p class="name">Jane Doe</p>
+            <p class="position">Instructor</p>
+        </div>
+        <div class="card-container">
+            <img class="team-member-image" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg" alt="Instructor">
+            <p class="name">John Smith</p>
+            <p class="position">Instructor</p>
         </div>
     </div>
 </div>
-
 </body>
 </html>
