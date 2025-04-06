01ca858dd9f582445a738d1fe177b881ef12ec2c
NataliaKlimaszewska <nataliaklimaszewska03@proton.me>
2025-04-06 16:58:28 +0200

    log in page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CAKE GENERATOR</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<header class="bg-white shadow-md">
+    <nav class="bg-white px-8 pt-2 shadow-md">
+        <div class="-mb-px flex justify-center">
+            <a class="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="home.php">Home</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="CreateRecipe.php">Create Your Own Recipe</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="Ideas.php">Ideas</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="LogginPage.php">Log In</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="SignInPage.php">Sign In</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="Quiz.php">Quiz</a>
+            <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3" href="OurTeam.php">Meet Our Team</a>
+        </div>
+    </nav>
+</header>
+
+<main class="p-8">
+    <p>Tutaj damy całą stronę startową</p>
+</main>
+    @yield("content")
+</body>
+</html>
