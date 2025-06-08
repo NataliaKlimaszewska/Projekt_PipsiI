595cdd75de4fe2060504287ebbd0ae6557ff3172
MarcinSzuszko <marcin.szuszko@studenci.collegiumwitelona.pl>
2025-06-09 00:33:26 +0200

    Merge repair

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -37,59 +37,6 @@
             </div>
 
         </div>
-
-    <!-- component -->
-    <div class="bg-pink-100 flex justify-center items-center h-screen">
-        <!-- Left: Image -->
-        <div class="w-1/2 h-screen hidden lg:block">
-            <img src="https://images.unsplash.com/photo-1625103709286-0ad26ab00b61?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Placeholder Image" class="object-cover w-full h-full">
-        </div>
-        <!-- Right: Login Form -->
-        <div class= "lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-            <h1 class="text-2xl font-semibold mb-4">Login</h1>
-            @if ($errors->any())
-                <div class="bg-red-200 p-4 rounded">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li class="text-red-800">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <form action="{{ route('logIn') }}" method="POST">
-                @csrf
-                @error('email')
-                <div class="alert alert-danger" role="alert">
-                    <span>{{ $message }}</span>
-                </div>
-                @enderror
-                <!-- Username Input -->
-                <div class="mb-4" "bg-sky-100">
-                    <label for="email" class="block text-gray-600">Email</label>
-                    <input type="text" id="email" name="email" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off">
-                </div>
-                <!-- Password Input -->
-                <div class="mb-4">
-                    <label for="password" class="block text-gray-800">Password</label>
-                    <input type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" autocomplete="off">
-                </div>
-                <!-- Remember Me Checkbox -->
-                <div class="mb-4 flex items-center">
-                    <input type="checkbox" id="remember" name="remember" class="text-red-500">
-                    <label for="remember" class="text-green-900 ml-2">Remember Me</label>
-                </div>
-                <!-- Forgot Password Link -->
-                <div class="mb-6 text-blue-500">
-                    <a href="#" class="hover:underline">Forgot Password?</a>
-                </div>
-                <!-- Login Button -->
-                <button type="submit" class="bg-red-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
-            </form>
-        <!-- Sign up  Link -->
-        <div class="mt-6 text-green-500 text-center">
-            <a href="#" class="hover:underline">Sign up Here</a>
-        </div>
-    </div>
     </div>
 
 @endsection
